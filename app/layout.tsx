08513eed7b9443b771161a4fd8a9f8431a427194
Keyan <keyanjavanshir@live.dk>
2024-02-29 21:13:27 +0100

    Set up user authentication using Clerk. Set up a .env.local file (secret). Configured middleware for Clerk. Styled clerk using the appearance variable and the .auth class in CSS.globals.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
 // Using IPM_Plex font
 // Set up meta data
@@ -22,11 +23,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-       {/* Import cn fron shadcn. Use IPMPlex font. */}
-      <body className={cn("font-IPMPlex antialiased", IBMPlex.variable)}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider appearance={{
+      variables: {colorPrimary: '#624cf5'}
+    }}>
+      <html lang="en">
+        {/* Import cn fron shadcn. Use IPMPlex font. */}
+        <body className={cn("font-IPMPlex antialiased", IBMPlex.variable)}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
